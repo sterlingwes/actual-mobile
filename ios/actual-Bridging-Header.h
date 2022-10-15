45c31d235c3e9122dd2476745ed1e204d0eb7559
Wes Johnson <wes@swj.io>
2022-10-15 14:30:05 -0400

    init mostly building
    had to do a few things so far: 1. rough in packages from the monorepo this was extracted from using local paths; 2. delete a bunch of node-in-app code which seems to only be used for sentry; 3. tweak some metro and babel build configs; 4. switch to yarn 1.x which is more standard in RN world; 5. fix the missing bridging header on iOS; 6. bunch more i suspect

---
+++ ios/actual-Bridging-Header.h
@@ -0,0 +1,10 @@
+#ifndef actual_Bridging_Header_h
+#define actual_Bridging_Header_h
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
+#import "React/RCTScrollView.h"
+#import "React/RCTUIManager.h"
+#import "React/RCTView.h"
+
+#endif /* actual_Bridging_Header_h */
---
+++ test/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts', 'json', 'testing.js'],
+  setupTestFrameworkScriptFile: './test/setup.js',
+  testEnvironment: 'node',
+  rootDir: '../',
+};
